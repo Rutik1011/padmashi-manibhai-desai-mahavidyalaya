a9f990c7750d0087bf1f1f8af5ff3b0e7ff73227
Rutik <rutikkale2001@gmail.com>
2025-11-05 19:05:29 +0530

    Delete Desktop/Personal Java/loginRegisterModuleUsingJpa directory

--- Desktop/Personal Java/loginRegisterModuleUsingJpa/src/main/java/com/repo/Orders.java
+++
@@ -1,9 +0,0 @@
-package com.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.entity.ShopHatOrder;
-
-public interface Orders extends JpaRepository<ShopHatOrder, Long> {
-         ShopHatOrder findByOrderId(String orderId);
-}
